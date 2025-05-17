24db6486e5d61838177b4b4172fc827627c64b89
sigantengbanget <jalaldalkhotami@gmail.com>
2025-05-17 11:03:37 +0700

    feat:add port

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import app from './app';
 import config from './config/config';
 
-// app.listen(config.port, () => {
-//   console.log(`Server running on port ${config.port}`);
-// });
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
 
 app.get(`/`, (req, res) => {
   res.send('Welcome to the BMR Server');
 });
-
-export default app;
