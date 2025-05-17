c2eb6d486a31f5303d760883769863acedf109a3
sigantengbanget <jalaldalkhotami@gmail.com>
2025-05-17 10:35:30 +0700

    feat:add ping

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors'
 import authRoutes from './route/authRoutes'
 import bookingRoutes from './route/bookingRoutes';
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/ping", (req: Request, res: Response) => {
+    res.send("pong");
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/booking', bookingRoutes);
